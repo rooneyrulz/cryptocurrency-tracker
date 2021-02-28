78835d03a6f004d2e9fbe4ef75ddcb74516293a0
ruzny <ruzny@ruzny.com>
2021-02-28 15:33:06 +0530

    fetching coins and rendering to the DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React from 'react';
+
 const App = () => {
-  return (
+  const [loading, setLoading] = React.useState(true);
+  const [coins, setCoins] = React.useState([]);
+
+  React.useEffect(() => {
+    getCoins();
+  }, []);
+
+  function getCoins() {
+    return fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setCoins(data);
+      })
+      .catch((error) => {
+        setLoading(false);
+        throw error;
+      });
+  }
+
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
     <div className='app'>
       <h1>Cryptocurrency Tracker</h1>
+      {coins.length ? (
+        coins.map((coin) => <h4 key={coin.symbol}>{coin.name}</h4>)
+      ) : (
+        <p>Coins not found...!</p>
+      )}
     </div>
   );
 };
